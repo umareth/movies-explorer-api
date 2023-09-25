7683e9768fe290d902d33f7211ef909c7cd5dea5
Maya <Maya@mail.ru>
2023-09-25 19:09:26 +0300

    upload fix

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,8 +1,8 @@
-const centralErrorHandler = (err, req, res) => {
+const centralErrorHandler = (err, req, res, next) => {
   // Если у ошибки нет статуса, выставляем 500 Internal Server Error
   const { statusCode = 500, message } = err;
 
-  // Отправляем ответ с соответствующим статусом и сообщением
+  // Отправляем JSON-ответ с соответствующим статусом и сообщением
   res.status(statusCode).json({ error: message });
 };
 
