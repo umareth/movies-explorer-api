b1fd12e46018b854079aeebaeb739ebfad850e5a
Maya <Maya@mail.ru>
2023-10-18 06:29:47 +0300

    upload_new_api

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,22 +1,28 @@
-const { DEFAULT_ALLOWED_METHODS, allowedCors } = require('../utils/config');
+const allowedCors = [
+  "http://shakh.nomoredomainsrocks.ru",
+  "https://shakh.nomoredomainsrocks.ru",
+  "http://localhost:3000",
+  "https://localhost:3000",
+  "http://localhost:3001",
+  "https://localhost:3001",
+];
 
-module.exports = (req, res, next) => {
+const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+const cors = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-
-  // Если источник запроса разрешен, устанавливаем заголовок Access-Control-Allow-Origin
+  const requestHeaders = req.headers["access-control-request-headers"];
+  res.header("Access-Control-Allow-Credentials", true);
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Origin", origin);
   }
-
-  // Если метод запроса - OPTIONS, устанавливаем заголовки CORS и завершаем ответ
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.header('Access-Control-Allow-Credentials', true);
-    res.status(204).end(); // 204 No Content
-  } else {
-    next();
+  if (method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
+    res.header("Access-Control-Allow-Headers", requestHeaders);
+    res.end();
+    return;
   }
+  next();
 };
+
+module.exports = cors;
